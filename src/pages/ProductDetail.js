0d49d300ce7a4a581c66908171cce7b2e6b6152c
Banti-kumar <Bantykumar13365@gmail.com>
2026-04-02 14:56:07 +0530

    update the locat storage

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,19 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
+import { getProducts } from "../services/api";
 
 function ProductDetail() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const { products, setProducts, addToCart } = useContext(CartContext);
 
-  const { products, addToCart } = useContext(CartContext);
+  const [loading, setLoading] = useState(false);
 
-  const product = products?.find((p) => p.id === Number(id));
+  useEffect(() => {
+    if (products.length === 0) {
+      setLoading(true);
+      getProducts().then((data) => {
+        setProducts(data);
+        setLoading(false);
+      });
+    }
+  }, [products, setProducts]);
 
-  if (!product) {
+  const product = products.find((p) => p.id === Number(id));
+
+  if (loading || !product) {
     return (
       <div style={{ padding: "20px" }}>
-        <h2>Loading or Product not found</h2>
+        <h2>Loading...</h2>
         <button onClick={() => navigate("/")}>Go Home</button>
       </div>
     );
@@ -24,7 +36,7 @@ function ProductDetail() {
       <button onClick={() => navigate("/")}>← Back</button>
 
       <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
-        <img src={product?.images?.[0]} alt={product?.title} width="300" />
+        <img src={product.images?.[0]} alt={product.title} width="300" />
 
         <div>
           <h2>{product.title}</h2>
